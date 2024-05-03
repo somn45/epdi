e580c7c45ecf0012a09c5055754d6a2c4785c4ed
unknown <somn45@gmail.com>
2024-05-03 16:51:13 +0900

    fix : companies/[name]/page.tsx에서 잘못된 Company 모델명을 사용하던 점 해결

--- app/companies/[name]/page.tsx
+++ app/companies/[name]/page.tsx
@@ -1,11 +1,10 @@
 import getCompany from "@/util/get-company";
-import ClientCompanyDetail from "./CompanyDetail";
-import Company from "@/models/Company";
 import Detail from "../Detail";
 import { CompanyType } from "@/app/types/company";
+import companyModel from "@/models/Company";
 
 export async function generateStaticParams() {
-  const companies = await Company.find();
+  const companies = await companyModel.find();
   return companies.map((company) => ({
     name: company.name,
   }));
