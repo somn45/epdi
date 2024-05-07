5d679781c07148d56ef1e74781f55a951e98713f
unknown <somn45@gmail.com>
2024-05-07 15:42:36 +0900

    fix : 배포 시 정적 렌더링에서 동적 렌더링이 되도록 변경

--- util/get-company.ts
+++ util/get-company.ts
@@ -1,8 +1,10 @@
-import connectDB from '@/config/connectDB';
-import companyModel from '@/models/Company';
-import 'server-only';
+import { unstable_noStore as noStore } from "next/cache";
+import connectDB from "@/config/connectDB";
+import companyModel from "@/models/Company";
+import "server-only";
 
 const getCompany = async (name: string) => {
+  noStore();
   await connectDB();
   const decodedName = decodeURI(name);
   const company = await companyModel.findOne({ name: decodedName }).lean();
