0ed0c753dc776793c7a1a7c8172cc8d2186e0305
unknown <somn45@gmail.com>
2024-05-07 15:49:12 +0900

    git : 기존에 배포되어 있던 저장소의 코드와 충돌 해결 ver.2

--- util/get-company.ts
+++ util/get-company.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { unstable_noStore as noStore } from "next/cache";
 import connectDB from "@/config/connectDB";
 import companyModel from "@/models/Company";
 import "server-only";
-=======
-import { unstable_noStore as noStore } from 'next/cache';
-import connectDB from '@/config/connectDB';
-import companyModel from '@/models/Company';
-import 'server-only';
->>>>>>> 85a57aa3821778583db2dc1d142fb1fb7b492f5d
 
 const getCompany = async (name: string) => {
   noStore();
